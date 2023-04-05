8e4cbd568630086e7219258bd78a88cb1d8d06ee
unknown <dnal03@msn.com>
2023-04-05 16:15:53 -0400

    maps-n-sets

---
+++ practicing-maps-n-sets/mapsAndSets.js
@@ -0,0 +1,19 @@
+let fruitInventory = new Map();
+fruitInventory.set('apple', 10);
+fruitInventory.set('banana', 20);
+fruitInventory.set('orange', 30);
+
+fruitInventory.set('apple', 15)
+
+let bananaInventory = fruitInventory.get('banana');
+
+let uniqueColors = new Set();
+uniqueColors.add('red');
+uniqueColors.add('blue');
+uniqueColors.add('green');
+
+uniqueColors.add('yellow');
+
+console.log(uniqueColors.has('blue'));
+
+let hasBlue = uniqueColors('blue');
