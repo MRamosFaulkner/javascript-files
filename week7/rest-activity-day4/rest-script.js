9bb82ffd34c9ba3db7359ab91b54b41f42ed5cf2
unknown <dnal03@msn.com>
2023-04-20 23:59:13 -0400

    corrections

--- week7/rest-activity-day4/rest-script.js
+++ week7/rest-activity-day4/rest-script.js
@@ -1,100 +1,111 @@
+const showResults = document.getElementById('results');
 
-const output = document.getElementById('output');
-
-function showResults(results) {
-    output.innerHTML = JSON.stringify(results, null, 2);
-}    
-
-// get all posts
-async function getAllPosts() {
-
-    const response = await fetch('http://jsonplaceholder.typicode.com/posts');
-    const posts = await response.json();
-
-    showResults(posts);
-}
-async function getPostsWithID10() {
-    
-    const response = await fetch('http://jsonplaceholder.typicode.com/posts/10');
-    const posts = await response.json();
-    
-    showResults(posts);
-}
-// async function createtNewPost() {
-
-//     const response = await fetch('http://jsonplaceholder.typicode.com/posts/", {
-//         method: 'POST',
-//         body: JSON.stringify({
-//           title: 'New Post',
-//           body: 'This is a new post',
-//           userId: 1,   
-//         }),
-//         headers: {
-//             'Content-type': 'application/json; charset=UTF-8',
-//         },
-//     }),
-// async    
-//     const posts = await response.json();
-
-//     showResults(posts);
-// }
-async function createNewPost() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-        body: JSON.stringify({
-            title: "New Post",
-            body: "This is a new post.",
-            userId: 1,
-        }),
+function getAllPosts() {
+  showResults.innerHTML = "";
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => {
+      for (let i = 0; i < json.length; i++) {
+        showResults.innerHTML += 
+          '<div>' +
+          '<h2>' + json[i].title + '</h2>' +
+          '<p>' + json[i].body + '</p>' +
+          '</div>';
+      }
     });
-    const post = await response.json();
-    showResults(post);
 }
 
-async function replacePostWithID12() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts/12", {
-        method: "PUT",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-        body: JSON.stringify({
-            id: 12,
-            title: "Replaced Post with ID 12",
-            body: "This the replaced post.",
-            userId: 1,
-        }),
+function getPostsWithID10() {
+  showResults.innerHTML = "";
+  fetch('http://jsonplaceholder.typicode.com/posts/10')
+    .then(response => response.json())
+    .then(post => {
+      showResults.innerHTML += 
+        '<div>' +
+        '<h2>' + post.title + '</h2>' +
+        '<p>' + post.body + '</p>' +
+        '</div>';
     });
-    const post = await response.json();
-    displayResult(post);
 }
 
-async function updatePost12Title() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts/12", {
-        method: "PATCH",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-        body: JSON.stringify({
-            title: "Updated Title for Post 12",
-        }),
+function createNewPost() {
+  showResults.innerHTML = "";
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      title: "New Post",
+      body: "This is a new post.",
+      userId: 1,
+    }),
+  })
+    .then(response => response.json())
+    .then(post => {
+      showResults.innerHTML += 
+        '<div>' +
+        '<h2>' + post.title + '</h2>' +
+        '<p>' + post.body + '</p>' +
+        '</div>';
     });
-    const post = await response.json();
-    displayResult(post);
 }
 
-async function deletePost12() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts/12", {
-        method: "DELETE",
+function replacePostWithID12() {
+  showResults.innerHTML = "";
+  fetch("https://jsonplaceholder.typicode.com/posts/12", {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      id: 12,
+      title: "Replaced Post with ID 12",
+      body: "This the replaced post.",
+      userId: 1,
+    }),
+  })
+    .then(response => response.json())
+    .then(post => {
+      showResults.innerHTML += 
+        '<div>' +
+        '<h2>' + post.title + '</h2>' +
+        '<p>' + post.body + '</p>' +
+        '</div>';
     });
-    if (response.ok) {
-        displayResult("Post with ID 12 successfully deleted.");
-    } else {
-        displayResult("Error deleting post with ID 12.");
-    }
 }
 
+function updatePost12Title() {
+  showResults.innerHTML = "";
+  fetch("https://jsonplaceholder.typicode.com/posts/12", {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      title: "Updated Title for Post 12",
+    }),
+  })
+    .then(response => response.json())
+    .then(post => {
+      showResults.innerHTML += 
+        '<div>' +
+        '<h2>' + post.title + '</h2>' +
+        '<p>' + post.body + '</p>' +
+        '</div>';
+    });
+}
 
-
+function deletePost12() {
+  showResults.innerHTML = "";
+  fetch("https://jsonplaceholder.typicode.com/posts/12", {
+    method: "DELETE",
+  })
+    .then(response => {
+      if (response.ok) {
+        showResults.innerHTML += "Post with ID 12 successfully deleted.";
+      } else {
+        showResults.innerHTML += "Error deleting post with ID 12.";
+      }
+    });
+}
